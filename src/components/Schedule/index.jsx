3f6c2588ca6c127baca545c8c1680a9ca582ce1f
Dmitry Gonokhov <gonokhov.dm@gmail.com>
2023-03-27 19:47:16 +0700

    fix schedule handler

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -1,144 +1,134 @@
-import React, { Component } from "react";
-import classNames from "classnames";
-// import { Route, Routes } from 'react-router-dom';
+import React, { useState } from "react";
 import ScheduleItem from "./ScheduleItem";
 import './Schedule.scss'
 
-class CheduleTabs extends Component {
+function CheduleTabs() {
 
-	state = {
-		activeItem: 1,
-	}
+	const [activeItem, setActiveItem] = useState(1)
 
-	handleItem = (e) => {
-		this.setState({
-			activeItem: +e.target.getAttribute('data-name')
-		})
-	}
+	const ScheduleTitle = [
+		{
+			id: 1,
+			day: "Monday",
+			date: "May 15, 2023"
+		},
+		{
+			id: 2,
+			day: "Tuesday",
+			date: "May 16, 2023"
+		},
+		{
+			id: 3,
+			day: "Wednesday",
+			date: "May 17, 2023"
+		}
+	]
 
-	render() {
-		const { activeItem } = this.state;
-		return (
-			<div className="schedule" >
-				<div className="schedule__title">
-					<h4>Our Timetable</h4>
-					<h2>Schedule plan</h2>
-				</div>
-				<div className="schedule__nav">
-					<ul>
-						<li
-							data-name={1}
-							onClick={this.handleItem}
-							className={classNames('schedule__nav-item', { active: this.state.activeItem === 1 })}
-						>
-							<span>Monday</span>
-							<p>May 15, 2023</p>
-						</li>
+	console.log(activeItem)
 
-						<li
-							data-name={2}
-							onClick={this.handleItem}
-							className={classNames('schedule__nav-item', { active: this.state.activeItem === 2 })}
-						>
-							<span>Tuesday</span>
-							<p>May 16, 2023</p>
-						</li>
-
-						<li
-							data-name={3}
-							onClick={this.handleItem}
-							className={classNames('schedule__nav-item', { active: this.state.activeItem === 3 })}
-						>
-							<span>Wednesday</span>
-							<p>May 17, 2023</p>
-						</li>
-					</ul>
-				</div>
-				{ activeItem === 1 && 
-					<div className="schedule__list">
-						<ScheduleItem 
-							image = 'schedule-1.jpg'
-							theme = 'Dealing with Difficult People'
-							author = 'Gary Armstrong'
-							date = '15 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-						<ScheduleItem 
-							image = 'schedule-2.jpg'
-							theme = 'Crop Insurance Conference'
-							author = 'Amanda Hudson'
-							date = '15-16 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-						<ScheduleItem 
-							image = 'schedule-3.jpg'
-							theme = 'Capdm Executive Conference'
-							author = 'Martha Burke'
-							date = '15-17 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-						<ScheduleItem 
-							image = 'schedule-4.jpg'
-							theme = 'Street Food Convention'
-							author = 'Jeffrey Morales'
-							date = '15-17 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-					</div>
-				} 
-
-				{ activeItem === 2 && 
-					<div className="schedule__list">
-						<ScheduleItem 
-							image = 'schedule-2.jpg'
-							theme = 'Crop Insurance Conference'
-							author = 'Amanda Hudson'
-							date = '15-16 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-						<ScheduleItem 
-							image = 'schedule-3.jpg'
-							theme = 'Capdm Executive Conference'
-							author = 'Martha Burke'
-							date = '15-17 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-						<ScheduleItem 
-							image = 'schedule-4.jpg'
-							theme = 'Street Food Convention'
-							author = 'Jeffrey Morales'
-							date = '15-17 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-					</div>
-				} 
-
-				{ activeItem === 3 && 
-					<div className="schedule__list">
-						<ScheduleItem 
-							image = 'schedule-3.jpg'
-							theme = 'Capdm Executive Conference'
-							author = 'Martha Burke'
-							date = '15-17 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-						<ScheduleItem 
-							image = 'schedule-4.jpg'
-							theme = 'Street Food Convention'
-							author = 'Jeffrey Morales'
-							date = '15-17 May, 2023'
-							place = 'Mountain Resort, Phoenix, USA'
-						/>
-					</div>
-				} 
-
-				<div className="schedule__download-btn">
-					<a href="/">Download schedule (PDF)</a>
-				</div>
+	return (
+		<div className="schedule" >
+			<div className="schedule__title">
+				<h4>Our Timetable</h4>
+				<h2>Schedule plan</h2>
 			</div>
-		)
-	}
+			<div className="schedule__nav">
+				<ul>
+					{ScheduleTitle.map(item => (
+						<li
+							key={item.id}
+							onClick={() => setActiveItem(item.id)}
+							className={`schedule__nav-item ${activeItem !== item.id ? "" : "active"}`}
+						>
+							<span>{item.day}</span>
+							<p>{item.date}</p>
+						</li>
+					))}
+				</ul>
+			</div>
+			{activeItem === 1 &&
+				<div className="schedule__list">
+					<ScheduleItem
+						image='schedule-1.jpg'
+						theme='Dealing with Difficult People'
+						author='Gary Armstrong'
+						date='15 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+					<ScheduleItem
+						image='schedule-2.jpg'
+						theme='Crop Insurance Conference'
+						author='Amanda Hudson'
+						date='15-16 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+					<ScheduleItem
+						image='schedule-3.jpg'
+						theme='Capdm Executive Conference'
+						author='Martha Burke'
+						date='15-17 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+					<ScheduleItem
+						image='schedule-4.jpg'
+						theme='Street Food Convention'
+						author='Jeffrey Morales'
+						date='15-17 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+				</div>
+			}
 
+			{activeItem === 2 &&
+				<div className="schedule__list">
+					<ScheduleItem
+						image='schedule-2.jpg'
+						theme='Crop Insurance Conference'
+						author='Amanda Hudson'
+						date='15-16 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+					<ScheduleItem
+						image='schedule-3.jpg'
+						theme='Capdm Executive Conference'
+						author='Martha Burke'
+						date='15-17 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+					<ScheduleItem
+						image='schedule-4.jpg'
+						theme='Street Food Convention'
+						author='Jeffrey Morales'
+						date='15-17 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+				</div>
+			}
+
+			{activeItem === 3 &&
+				<div className="schedule__list">
+					<ScheduleItem
+						image='schedule-3.jpg'
+						theme='Capdm Executive Conference'
+						author='Martha Burke'
+						date='15-17 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+					<ScheduleItem
+						image='schedule-4.jpg'
+						theme='Street Food Convention'
+						author='Jeffrey Morales'
+						date='15-17 May, 2023'
+						place='Mountain Resort, Phoenix, USA'
+					/>
+				</div>
+			}
+
+			<div className="schedule__download-btn">
+				<a href="/">Download schedule (PDF)</a>
+			</div>
+		</div>
+	)
 }
 
 export default CheduleTabs;
