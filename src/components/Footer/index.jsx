459b6f18789ee41c5ff718a5971286bbc518d23c
Dmitry Gonokhov <gonokhov.dm@gmail.com>
2023-03-28 21:34:05 +0700

    upgrade logo and person cards

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -18,8 +18,19 @@ function Footer() {
 				<div className="row gx-5">
 					<div className="col-12 col-lg-3">
 						<div className="footer-col">
-							<a href="/">
-								<div className="footer-col__title">CONFER</div>
+							<a href='/' className="footer-col__logo">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 512 512">
+									<defs>
+										<linearGradient id="MyGradient" gradientTransform="rotate(60)">
+											<stop offset="10%" stopColor="#df42b1" />
+											<stop offset="95%" stopColor="#734bbb" />
+										</linearGradient>
+									</defs>
+									<path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z" />
+								</svg>
+								<span className="nav-logo__text">CONFER</span>
 							</a>
 							<p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain.</p>
 							<div className="footer-col__social">
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,21 +3,34 @@ import './Navbar.scss';
 import Button from '../Button';
 
 function Navbar() {
-  return (
-    <div className="container jc-space-between ai-center">
-      <span className="nav-logo">CONFER</span>
-      <ul>
-        <li>Home</li>
-        <li>Pages</li>
-        <li>Speakers</li>
-        <li>Blog</li>
-        <li>Contact</li>
-        <li>
-          <Button title="Get tickets"/>
-        </li>
-      </ul>
-    </div>
-  )
+	return (
+		<div className="container jc-space-between ai-center">
+			<a href='/' className='nav-logo'>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 512 512">
+					<defs>
+						<linearGradient id="MyGradient" gradientTransform="rotate(60)">
+							<stop offset="10%" stopColor="#df42b1" />
+							<stop offset="95%" stopColor="#734bbb" />
+						</linearGradient>
+					</defs>
+					<path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z" />
+				</svg>
+				<span className="nav-logo__text">CONFER</span>
+			</a>
+			<ul>
+				<li>Home</li>
+				<li>Pages</li>
+				<li>Speakers</li>
+				<li>Blog</li>
+				<li>Contact</li>
+				<li>
+					<Button title="Get tickets" />
+				</li>
+			</ul>
+		</div>
+	)
 }
 
 export default Navbar
